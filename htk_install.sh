64f3ec83f41d961780f46ec571c013084b70ce20
Zoldur <36329578+zoldur@users.noreply.github.com>
2018-03-01 23:36:02 +0000

    Update htk_install.sh

--- htk_install.sh
+++ htk_install.sh
@@ -24,7 +24,7 @@ function compile_node() {
   cd $TMP_FOLDER
   wget -q $COIN_REPO 
   compile_error 
-  unzip HTK-2.1.0.3.zip >/dev/null 2>&1
+  unzip 2.1.0.3.zip >/dev/null 2>&1
   compile_error 
   cp htk* /usr/local/bin
   cd -
@@ -200,7 +200,7 @@ apt-get update >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
 libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget pwgen curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev >/dev/null 2>&1
+libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev unzip >/dev/null 2>&1
 if [ "$?" -gt "0" ];
   then
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
@@ -210,7 +210,7 @@ if [ "$?" -gt "0" ];
     echo "apt-get update"
     echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
 libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git pwgen curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev"
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev unzip"
  exit 1
 fi
 
